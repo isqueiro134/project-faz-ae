91dce91045c5ec54df283c7f1150e78571a06a46
Isaque Oliveira <isaque.om@sempreceub.com>
2026-04-13 00:44:02 -0300

    feat(repo): implement repository pattern for data access

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ import { supabase } from './src/config/db.js';
 const app = express();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+
 app.use(express.json());
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.status(200).sendFile(path.join(__dirname, 'src/public/pages/home.html'));
@@ -20,8 +22,8 @@ app.post('/users', async (req, res) => {
     }
 
     const { data, error } = await supabase
-    .from('users')
-    .insert([{ name, email, password, user_type: userType }]);
+        .from('users')
+        .insert([{ name, email, password, user_type: userType }]);
 
     if (error) {
         return res.status(500).json({ error: error.message });
--- src/config/db.js
+++
@@ -1,5 +0,0 @@
-import dotenv from 'dotenv';
-import { createClient } from '@supabase/supabase-js';
-dotenv.config();
-
-export const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
