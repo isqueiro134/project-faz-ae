5c58b09ab028e8042e2ef806eb0a3ca7448e98f2
Isaque Oliveira <isaque.om@sempreceub.com>
2026-04-13 17:43:24 -0300

    refactor(backend): move view routes to viewRouter.js

--- app.js
+++ app.js
@@ -2,36 +2,14 @@ import 'dotenv/config';
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import viewRouter from './backend/routes/viewRouter.js';
 
 const app = express();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 app.use(express.json());
 app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, 'public/pages/home.html'));
-});
-
-app.get('/login', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, 'public/pages/login.html'));
-});
-
-app.get('/cadastro', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, 'public/pages/cadastro.html'));
-});
-
-app.get('/recuperar-senha', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, 'public/pages/recuperarSenha.html'));
-});
-
-app.get('/onboarding', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, 'public/pages/onboarding.html'));
-});
-
-app.get('/dashboard', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, 'public/pages/dashboard.html'));
-});
+app.use("/", viewRouter);
 
 const PORT = process.env.PORT;
 
